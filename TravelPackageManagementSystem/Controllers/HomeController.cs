d3113e60b820e52b8fea63051e30b89e3da7089c
Rani <2463475@cognizant.com>
2025-12-24 11:03:05 +0530

    hero section

--- TravelPackageManagementSystem/Controllers/HomeController.cs
+++ TravelPackageManagementSystem/Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace TravelPackageManagementSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Hero()
+        {
+            return View();
+        }
     }
 }

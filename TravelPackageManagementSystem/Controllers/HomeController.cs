ecb278c6f4de6c763c0c33e2bed14604cc4b94e6
Lina Patil <2464051@cognizant.com>
2025-12-29 23:32:33 +0530

    Booking 9 packages

--- TravelPackageManagementSystem/Controllers/HomeController.cs
+++ TravelPackageManagementSystem/Controllers/HomeController.cs
@@ -55,11 +55,7 @@ namespace TravelPackageManagementSystem.Controllers
             ViewBag.PackageId = id;
             return View();
         }
-        public IActionResult KeralaTD(string id)
-        {
-            ViewBag.PackageId = id;
-            return View();
-        }
+       
         public IActionResult MizoramTD(string id)
         {
             ViewBag.PackageId = id;
@@ -91,7 +87,49 @@ namespace TravelPackageManagementSystem.Controllers
         {
             return View();
         }
+        public IActionResult KeralaPack1()
+        {
+            return View(); 
+        }
+        public IActionResult KeralaPack2()
+        {
+            return View();
+        }
+        public IActionResult KeralaPack3()
+        {
+            return View();
+        }
+        public IActionResult KeralaTD(string id)
+        {
         
+            ViewBag.PackageId = id;
+            return View();
+        }
+        public IActionResult TamilPack1()
+        {
+            return View();
+        }
+        public IActionResult TamilPack2()
+        {
+            return View();
+        }
+        public IActionResult TamilPack3()
+        {
+            return View();
+        }
+        public IActionResult MizoPack3()
+        {
+            return View();
+        }
+        public IActionResult MizoPack2()
+        {
+            return View();
+        }
+        public IActionResult MizoPack1()
+        {
+            return View();
+        }
+
 
         public IActionResult Manali()
         {
